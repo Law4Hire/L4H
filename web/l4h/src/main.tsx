ee896e1bc032161c12910fde4372273105b2c58b
Law4Hire <usimmigrationhelp.law4hire@gmail.com>
2025-10-10 14:24:40 -0400

    Add basename to BrowserRouter for /law4hire/ deployment

--- web/l4h/src/main.tsx
+++ web/l4h/src/main.tsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider, QueryProvider, I18nProvider, i18n } from '@l4h/shared-ui'
+import { ThemeProvider, QueryProvider, I18nProvider, i18n, loadSupportedCultures } from '@l4h/shared-ui'
 import App from './App'
 import './index.css'
 
 // Ensure i18n is initialized
 console.log('i18n initialized:', i18n.isInitialized)
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <QueryProvider>
-          <I18nProvider>
-            <App />
-          </I18nProvider>
-        </QueryProvider>
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-)
+// Load supported cultures from the API
+loadSupportedCultures().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <BrowserRouter basename="/law4hire">
+        <ThemeProvider>
+          <QueryProvider>
+            <I18nProvider>
+              <App />
+            </I18nProvider>
+          </QueryProvider>
+        </ThemeProvider>
+      </BrowserRouter>
+    </React.StrictMode>
+  )
+});
