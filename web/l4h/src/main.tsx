3602db323fee67ca50d2d0b0f61776d940e062e7
Law4Hire <usimmigrationhelp.law4hire@gmail.com>
2025-10-10 14:25:30 -0400

    Remove unused i18n import to fix build

--- web/l4h/src/main.tsx
+++ web/l4h/src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider, QueryProvider, I18nProvider, i18n, loadSupportedCultures } from '@l4h/shared-ui'
+import { ThemeProvider, QueryProvider, I18nProvider, loadSupportedCultures } from '@l4h/shared-ui'
 import App from './App'
 import './index.css'
-
-// Ensure i18n is initialized
-console.log('i18n initialized:', i18n.isInitialized)
 
 // Load supported cultures from the API
 loadSupportedCultures().then(() => {
