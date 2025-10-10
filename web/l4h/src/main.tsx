4c2b569d2964d0208055ae106168f2eb18b29fc2
Law4Hire <usimmigrationhelp.law4hire@gmail.com>
2025-10-10 14:26:44 -0400

    Simplify main.tsx - remove loadSupportedCultures call

--- web/l4h/src/main.tsx
+++ web/l4h/src/main.tsx
@@ -1,23 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider, QueryProvider, I18nProvider, loadSupportedCultures } from '@l4h/shared-ui'
+import { ThemeProvider, QueryProvider, I18nProvider } from '@l4h/shared-ui'
 import App from './App'
 import './index.css'
 
-// Load supported cultures from the API
-loadSupportedCultures().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <BrowserRouter basename="/law4hire">
-        <ThemeProvider>
-          <QueryProvider>
-            <I18nProvider>
-              <App />
-            </I18nProvider>
-          </QueryProvider>
-        </ThemeProvider>
-      </BrowserRouter>
-    </React.StrictMode>
-  )
-});
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/law4hire">
+      <ThemeProvider>
+        <QueryProvider>
+          <I18nProvider>
+            <App />
+          </I18nProvider>
+        </QueryProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+)
